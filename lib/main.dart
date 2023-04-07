3e849f9e02239137c5151464dc00e60b874ac028
yasasvi <yasasvigumma@gmail.com>
2023-04-07 23:23:07 +0530

    job done

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() async {
   //* hive
   await Hive.initFlutter();
-  //* open box
+  //* open boxconst HomePage(),
   var box = await Hive.openBox('mybox');
 
   runApp(const MyApp());
@@ -18,7 +19,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      home: AnimatedSplashScreen(
+        splash: Icons.add_sharp,
+        duration: 5000,
+        backgroundColor: Color.fromARGB(255, 247, 247, 108),
+        splashTransition: SplashTransition.fadeTransition,
+        nextScreen: const HomePage(),
+      ),
       theme: ThemeData(primarySwatch: Colors.yellow),
     );
   }
